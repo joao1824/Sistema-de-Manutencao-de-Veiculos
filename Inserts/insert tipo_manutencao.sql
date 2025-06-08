be897ed1f97f5dbf7381e43d387bcbef889a1679
Gabriel Tramontin <gabrieltramontinaguiar@gmail.com>
2025-06-08 14:58:56 -0300

    Create insert tipo_manutencao.sql

---
+++ Inserts/insert tipo_manutencao.sql
@@ -0,0 +1,14 @@
+INSERT INTO tipo_manutencao (cd_tipo, descricao) 
+VALUES 
+(1, 'Troca de óleo'),
+(2, 'Troca de pneu'),
+(3, 'Troca de bateria'),
+(4, 'Verificação de freios'),
+(5, 'Alinhamento e balanceamento'),
+(6, 'Troca de pastilhas de freio'),
+(7, 'Troca de amortecedores'),
+(8, 'Troca de velas de ignição'),
+(9, 'Verificação do sistema de arrefecimento'),
+(10, 'Troca de embreagem'),
+(11, 'Verificação do sistema de escapamento'),
+(12, 'Revisão completa');
