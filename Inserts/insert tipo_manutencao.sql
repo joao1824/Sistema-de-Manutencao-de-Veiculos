7e6befa3004adad518edab18d440639e75ae14eb
Gabriel Tramontin <gabrieltramontinaguiar@gmail.com>
2025-06-08 15:02:41 -0300

    Update insert tipo_manutencao.sql

--- Inserts/insert tipo_manutencao.sql
+++ Inserts/insert tipo_manutencao.sql
@@ -4,11 +4,10 @@ VALUES
 (2, 'Troca de pneu'),
 (3, 'Troca de bateria'),
 (4, 'Verificação de freios'),
-(5, 'Alinhamento e balanceamento'),
-(6, 'Troca de pastilhas de freio'),
-(7, 'Troca de amortecedores'),
-(8, 'Troca de velas de ignição'),
-(9, 'Verificação do sistema de arrefecimento'),
-(10, 'Troca de embreagem'),
-(11, 'Verificação do sistema de escapamento'),
-(12, 'Revisão completa');
+(5, 'Troca de pastilhas de freio'),
+(6, 'Troca de amortecedores'),
+(7, 'Troca de velas de ignição'),
+(8, 'Verificação do sistema de arrefecimento'),
+(9, 'Troca de embreagem'),
+(10, 'Verificação do sistema de escapamento'),
+(11, 'Revisão completa');
