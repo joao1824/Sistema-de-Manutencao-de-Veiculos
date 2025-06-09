d8e13a08f3c39ffa0ef6f40f975ecd95bcd980de
Gabriel Reis Klein <gabrielreisklein@gmail.com>
2025-06-09 19:32:34 -0300

    Update insertAlas.sql
    Atualizando tabela alas

--- Inserts/insertAlas.sql
+++ Inserts/insertAlas.sql
@@ -1,15 +1,6 @@
 insert into alas (cd_alas, ala, descricao) 
-values (1, 'Administrativo', 'Local de controle financeiro, notas fiscais, controle de estoque e pedidos de peças.'),
-(2, 'Almoxarifado', 'Guarda pecas, ferramentas, oleos, pneus, filtros, etc.'),
-(3, 'Recepcao', 'Onde o cliente entra, faz o agendamento, explica o problema e aprova orcamentos.'),
-(4, 'Lavagem e Estética Automotiva', 'Para serviços de lavagem, polimento, cristalizacao, etc.'), 
-(5, 'Ala de Diagnóstico', 'Area equipada com scanner automotivo, multimetros e outros aparelhos para fazer diagnosticos iniciais nos veiculos.'),
-(6, 'Funilaria e Pintura', 'Voltado para pintura de veiculos e reparo de carroceria'),
-(7, 'Ala de Elétrica e Injeção Eletrônica', 'Voltada para diagnostico e reparo de problemas eletricos, sensores, ECU, injecao de combustivel, etc.'),
-(8, 'Refeitorio', 'Espaço pequeno com café, salgados ou vending machines para funcionários.'),
-(9, 'Vestiário', 'Para uso dos mecanicos: armarios, banheiro, chuveiros'),
-(10, 'Loja de Som e Personalizacao', 'Instalacao de som automotivo, alarmes, insulfilm, farois de LED, engates, etc.'),
-(11, 'Espaco de Treinamento', 'Para capacitação da equipe e treinamentos de marcas/parceiros');
-
-
-
+values (1, 'Norte', 'Ala de diagnosticos dos carros'),
+       (2, 'Sul', 'Ala de pintura e reparo de carroceria'),
+       (3, 'Leste', 'Instalacao de som automotivo, alarmes, insulfilme, farois de LED, engates, etc.'),
+       (4, 'Oeste', 'Para serviços de lavagem, polimento, cristalizacao, etc.'), 
+       (5, 'Centro', 'Reparo de problemas eletricos, sensores, ECU, injecao de combustivel, etc.'),
