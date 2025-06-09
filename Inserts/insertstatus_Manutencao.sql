8d4ac5a28d70e943d4d23c2916387bea9e20beb2
Gabriel Reis Klein <gabrielreisklein@gmail.com>
2025-06-09 19:20:41 -0300

    Create insertstatus_Manutencao.sql

---
+++ Inserts/insertstatus_Manutencao.sql
@@ -0,0 +1,11 @@
+insert into status_manutencoes (cd_status_manutencoes, status)
+values (1, 'Pronto'),
+       (2, 'Em Andamento'),
+       (3, 'Atrasado'),
+       (4, 'Em espera de pecas'),
+       (5, 'Em espera de pagamento'),
+       (6, 'Esperando o Seguro'),
+       (7, 'Carro não veio'),
+       (8, 'Em segundo Plano'),
+       (9, 'Emergencia'),
+       (10, 'Sem espaco para armazenar');
