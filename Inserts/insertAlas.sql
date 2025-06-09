3774144eab7d8d55857cb10c9fd3f086f75bd201
Gabriel Reis Klein <gabrielreisklein@gmail.com>
2025-06-09 19:37:27 -0300

    Update insertAlas.sql

--- Inserts/insertAlas.sql
+++ Inserts/insertAlas.sql
@@ -1,6 +1,6 @@
 insert into alas (cd_alas, ala, descricao) 
-values (1, 'Norte', 'Ala de diagnosticos dos carros'),
-       (2, 'Sul', 'Ala de pintura e reparo de carroceria'),
-       (3, 'Leste', 'Instalacao de som automotivo, alarmes, insulfilme, farois de LED, engates, etc.'),
-       (4, 'Oeste', 'Para serviços de lavagem, polimento, cristalizacao, etc.'), 
-       (5, 'Centro', 'Reparo de problemas eletricos, sensores, ECU, injecao de combustivel, etc.'),
+values (1, 'Norte', 'Ala da Equipe 1'),
+       (2, 'Sul', 'Ala de revisao'),
+       (3, 'Leste', 'Ala da Equipe 2'),
+       (4, 'Oeste', 'Ala da Equipe 3'), 
+       (5, 'Centro', 'Ala principal'),
