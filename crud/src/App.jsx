f91f1b3ebf86a822e4bbf750985a0ca891f402b5
🌑João Fogaça (SC/ALUNO SATC)🌑 <joao.1824@alunosatc.edu.br>
2025-06-11 10:23:41 -0300

    adicionando o Crud feito em React + a API

---
+++ crud/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ManutencoesList from './ManutencoesList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sistema de Manutenções</h1>
+      <ManutencoesList />
+    </div>
+  );
+}
+
+export default App;
