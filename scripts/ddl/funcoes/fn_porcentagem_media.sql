aee7857181a9470af2b84281a3e93af80f670b6c
🌑João Fogaça (SC/ALUNO SATC)🌑 <joao.1824@alunosatc.edu.br>
2025-06-20 22:17:01 -0300

    Create fn_porcentagem_media.sql

---
+++ scripts/ddl/funcoes/fn_porcentagem_media.sql
@@ -0,0 +1,14 @@
+-- Função usada para conseguir a % de comissão do funcionario com base em quanto tempo eles esta na empressa
+
+create or alter function fn_porcentagem_media (@valor numeric(5,2),@cd_funcionario int) returns numeric(5,2) as 
+	begin
+		declare @resposta numeric(5,2);
+		declare @tempo_entrada date;
+		declare @tempo_total int;
+		set @tempo_entrada = (select data_entrada from funcionarios where cd_funcionario = @cd_funcionario);
+		set @tempo_total = floor(datediff(year,@tempo_entrada,getdate()));
+		set @resposta = (@valor * (@tempo_total*10))/100;
+
+		return @resposta
+
+	end
