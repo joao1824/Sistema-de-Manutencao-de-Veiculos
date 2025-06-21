2c4d06a7329bf460cd5047608bf0ce3996b24aa2
Gabriel Reis Klein <gabrielreisklein@gmail.com>
2025-06-21 14:50:58 -0300

    Update pr_VerificaSeguro.sql

--- scripts/ddl/procedures/pr_VerificaSeguro.sql
+++ scripts/ddl/procedures/pr_VerificaSeguro.sql
@@ -20,31 +20,3 @@ create or alter procedure pr_VerificaSeguro as
         seguros ON veiculos.cd_seguro = seguros.cd_seguro
     WHERE
         manutencoes.cd_status_manutencoes = 6
-
-
-
-
-CREATE PROCEDURE CarrosAguardandoSeguro
-AS
-BEGIN
-    SELECT
-        m.placa,
-        sg.nome AS NomeSeguradora,
-        CASE 
-            WHEN sg.status = 1 THEN 'Ativa'
-            WHEN sg.status = 2 THEN 'Inativa'
-            ELSE 'Desconhecido'
-        END AS StatusSeguradora,
-        CASE 
-            WHEN sg.status = 2 THEN CAST(m.vl_manutencao * 1.15 AS NUMERIC(6,2))
-            ELSE m.vl_manutencao
-        END AS ValorManutencao
-    FROM
-        manutencoes m
-    INNER JOIN
-        carros c ON m.placa = c.placa
-    INNER JOIN
-        seguradoras sg ON c.id_seguradora = sg.id_seguradora
-    WHERE
-        m.cd_status_manutencoes = 6
-END;
