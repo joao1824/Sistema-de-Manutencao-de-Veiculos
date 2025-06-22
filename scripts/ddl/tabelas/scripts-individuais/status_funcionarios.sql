2094807273fc70d8ac2e5122435a5688784b6b09
🌑João Fogaça (SC/ALUNO SATC)🌑 <163940693+joao1824@users.noreply.github.com>
2025-06-22 12:37:14 -0300

    Update status_funcionarios.sql

--- scripts/ddl/tabelas/scripts-individuais/status_funcionarios.sql
+++ scripts/ddl/tabelas/scripts-individuais/status_funcionarios.sql
@@ -4,7 +4,7 @@
 
 
 create table status_funcionarios(
-	cd_status_funcionarios int not null primary key,
-	status varchar(10) not null check(status in ('Ativo','Suspenso','Demitido','Férias'))
+	cd_status_funcionarios tinyint not null primary key,
+	status varchar(10) not null check(status in ('Ativo','Suspenso','Demitido','FÃ©rias'))
 )
 go
