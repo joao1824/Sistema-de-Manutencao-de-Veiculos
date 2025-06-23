1eda7efc1a825f75ae29aad7b3f2d20f73d69841
Dr.Strangelove <163940822+DoppelsoldnerHRE@users.noreply.github.com>
2025-06-22 21:16:36 -0300

    Update IDX-Geral.sql

--- scripts/ddl/indices/IDX-Geral.sql
+++ scripts/ddl/indices/IDX-Geral.sql
@@ -23,4 +23,9 @@ create nonclustered index idx_func_fn_porcentagem_media on funcionarios (cd_func
 create nonclustered index idx_func_relatorio_top3 on funcionarios (cd_funcionario, cd_status_funcionario) include (nm_funcionario);
 create nonclustered index idx_manut_relatorio_top3 on manutencoes(cd_funcionario) include (vl_manutencao);
 
+
+CREATE NONCLUSTERED INDEX IX_funcionarios_sexo_status ON funcionarios (sexo, cd_status_funcionario) INCLUDE (cd_funcionario, nm_funcionario, data_saida, data_entrada);
+CREATE NONCLUSTERED INDEX IX_funcionarios_data_entrada ON funcionarios (data_entrada) INCLUDE (cd_funcionario);
+CREATE NONCLUSTERED INDEX IX_manutencoes_funcionario_valor ON manutencoes (cd_funcionario) INCLUDE (vl_manutencao);
+
 go
