3dc2425d6716ec53ce4826dfbe673430982181d5
Dr.Strangelove <163940822+DoppelsoldnerHRE@users.noreply.github.com>
2025-06-22 21:14:53 -0300

    Update idx-manutencoes.sql

--- scripts/ddl/indices/por-tabela/idx-manutencoes.sql
+++ scripts/ddl/indices/por-tabela/idx-manutencoes.sql
@@ -2,4 +2,5 @@ create nonclustered index idx_manutencoes_placa on manutencoes(placa)
 create nonclustered index idx_manutencoes_cd_funcionario on manutencoes(cd_funcionario)
 create nonclustered index idx_manutencoes_vl_manutencao on manutencoes(vl_manutencao)
 create nonclustered index idx_manut_relatorio_top3 on manutencoes (cd_funcionario) include (vl_manutencao);
+CREATE NONCLUSTERED INDEX IX_manutencoes_funcionario_valor ON manutencoes (cd_funcionario) INCLUDE (vl_manutencao);
 go
