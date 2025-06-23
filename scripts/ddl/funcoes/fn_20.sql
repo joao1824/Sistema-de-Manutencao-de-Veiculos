016bc6c54eeefe15bed1e8db16263304e03d2f40
Bruno Pagani Rampinelli <brunorampinelli2006@gmail.com>
2025-06-22 22:23:16 -0300

    Update tipo numeric

--- scripts/ddl/funcoes/fn_20.sql
+++ scripts/ddl/funcoes/fn_20.sql
@@ -1,8 +1,8 @@
 --Aplica um desconto de 20% sobre o valor informado.
 
-create or alter function fn_20 (@valor numeric(15,2)) returns numeric(15,2) as  
+create or alter function fn_20 (@valor numeric(7,2)) returns numeric(7,2) as  
 	begin
-		declare @descontado numeric(15,2);
+		declare @descontado numeric(7,2);
 		set @descontado = @valor * 0.8;
 		return @descontado;
 	end
